1123fa22f649f952486e520cdc649cd040065eba
Aimnor <marion.fabre@quandela.com>
2024-02-03 09:58:49 -0500

    remove get_cz method

--- main.py
+++ main.py
@@ -3,6 +3,3 @@ import perceval as pcvl
 
 def get_CCZ() -> pcvl.Processor:
     return pcvl.catalog["postprocessed ccz"].build_processor()
-
-def get_CZ() -> pcvl.Processor:
-    return pcvl.Processor("SLOS",6).add(0, pcvl.Unitary(pcvl.Matrix.eye(6)))
